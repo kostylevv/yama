3440cc94efc2d223e58887d473ace1869ecaf8ba
Vladislav Kostylev <kostylev.v@gmail.com>
2017-02-13 14:50:51 +0300

    Initial

---
+++ app/src/main/java/com/kostylevv/yama/MainActivity.java
@@ -0,0 +1,13 @@
+package com.kostylevv.yama;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
